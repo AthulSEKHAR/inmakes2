6b5612e8a50a3a397082321275e42424ea2179ce
AthulSEKHAR <117563215+AthulSEKHAR@users.noreply.github.com>
2024-02-08 18:29:29 +0530

    Add files via upload

---
+++ movieproject/movieproject/movieapp/urls.py
@@ -0,0 +1,16 @@
+
+from django.urls import path
+
+from  . import views
+
+app_name='movieapp'
+
+urlpatterns = [
+    path('', views.index,name='index'),
+    path('movie/<int:movie_id>/', views.detail, name='detail'),
+    path('add/', views.add, name='add'),
+    path('edit/<int:id>/', views.update, name='edit'),
+    path('delete/<int:id>/', views.delete, name='delete'),
+
+
+]
